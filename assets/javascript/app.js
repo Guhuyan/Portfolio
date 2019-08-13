d2b29750d1b54bdf8412eedeae3ea2d2e850a508
Gue <guehyang@gmail.com>
2019-08-12 23:05:49 -0700

    Not done, but submitting what I have so far.

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+/*
+// Add Clock
+
+// Scroll functions for menu
+$(document).ready(function () {
+    $("#menu-home").on("click", function() {
+        $("html, body").animate({
+            scrollTop: $(".article").offset().top
+        }, 'slow');
+    });
+    $("#menu-about").on("click", function() {
+        $("html, body").animate({
+            scrollTop: $(".about").offset().top
+        }, 'slow');
+    });
+    $("#menu-portfolio").on("click", function() {
+        $("html, body").animate({
+            scrollTop: $(".portfolio").offset().top
+        }, 'slow');
+    });
+    $("#menu-contact").on("click", function() {
+        $("html, body").animate({
+            scrollTop: $(".contact").offset().top
+        }, 'slow');
+    });
+});
+*/
